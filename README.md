3e37829aca7c2926eaf41da179b6037e66761195
Andrej Petras <andrej@lorislab.org>
2023-03-24 23:12:22 +0100

    feat: create changelog install action

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# install-changelog-action
-Install changelog action
+## Example usage
+
+```
+- uses: lorislab/install-changelog-action@v1
+- run: changelog version
+```
+
+```
+- uses: lorislab/install-changelog-action@v1
+  with:
+    version: "1.6.0"
+- run: changelog version
+```
+
+## Development
+
+#### Local development
+
+Install the dependencies
+
+```bash
+npm install
+```
+
+#### Package for distribution
+
+Run prepare
+
+```bash
+npm run prepare
+```
---
+++ index.js
@@ -0,0 +1,44 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const tc = require('@actions/tool-cache');
+
+
+async function run() {
+
+  try {
+    
+    let version = core.getInput('version');
+    let downloadURL = core.getInput('download_url');
+
+    const fillTemplate = function(s) {
+      s = s.replace(/\$\{version\}/g, version)
+      return s
+    }
+    
+    downloadURL = fillTemplate(downloadURL);
+    core.info(`samo donwload URL: ${downloadURL}`)
+    
+    await installTool(version, downloadURL);    
+    await exec.exec('samo version');
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+async function installTool(version, url) {
+  let cachedPath = tc.find('changelog', version)
+  if (cachedPath) {
+      core.addPath(cachedPath)
+      return
+  }
+
+  const file = await tc.downloadTool(url);
+
+  await exec.exec(`mkdir changelog`)
+  await exec.exec(`tar -C changelog -xzvf ${file} --strip-components 0 --wildcards samo`)
+
+  cachedPath = await tc.cacheDir('changelog', 'changelog', version);
+  core.addPath(cachedPath)
+}
+
+run();
